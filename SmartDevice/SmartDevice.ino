9387341890b077856874e7bc7180cf73b513478f
Glovania <9937412@schoolsnet.act.edu.au>
2023-05-15 10:16:04 +1000

    Updated 'SmartDevice.ino'
    Implament high-orders of code.

--- SmartDevice/SmartDevice.ino
+++ SmartDevice/SmartDevice.ino
@@ -8,10 +8,12 @@ RTC_Millis rtc;     // Software Real Time Clock (RTC)
 DateTime rightNow;  // used to store the current time.
 
 // SD Card - Confirm Pin
-#define SDpin 53
+#define SDpin 10
 
 void setup() {
+
   // put your setup code here, to run once:
+  
   Serial.begin(9600);  // Open serial communications and wait for port to open:
   while (!Serial) {
     delay(1);  // wait for serial port to connect. Needed for native USB port only
@@ -27,5 +29,88 @@ void setup() {
 }
 
 void loop() {
+
   // put your main code here, to run repeatedly:
+
+  RotaryPotentiometer();
+
+  delay(500);
+
+}
+
+/*
+
+Using the Potentiometer, determines at which bytes the device will start to opearated.
+@params none.
+@return none.
+
+*/
+void RotaryPotentiometer() {
+
+}
+
+/*
+
+If the Servo Motor is activated, rotate the Solar Panel so that it can observe tje Sun in multi-direction.
+@params none.
+@return none.
+
+*/
+void servoMotor() {
+
+}
+
+/*
+
+If the LDRs are activated, detects the highest possible light levels.
+@params none.
+@return none.
+
+*/
+void LDR() {
+
+}
+
+/*
+
+If the Crash Sensor being pressed, operates the Solar Panel.
+@params none.
+@return none.
+
+*/
+void crashSensor() {
+
+}
+
+
+/*
+
+When the Solar Panel started to observing and converting the Sun energies into electricity, shows the amount of it onto a LED displayer.
+@params none.
+@return none.
+
+*/
+void LED_Display() {
+
+}
+
+
+/*
+
+When the Solar Panel detected where the Sun is, play a confirm sound from the Buzzer.
+@params none.
+@return none.
+
+*/
+void buzzerButton() {
+
+}
+
+/*
+
+If the Solar Tracker is activated, starts detecting the 
+
+*/
+void SolarTracker() {
+
 }
