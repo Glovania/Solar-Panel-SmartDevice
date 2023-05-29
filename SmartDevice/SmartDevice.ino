1faf4f0ccfacf0adbbcf592a0cc9ccac12597a38
Glovania <9937412@schoolsnet.act.edu.au>
2023-05-29 11:18:28 +1000

    ✨feature: Update 'SmartDevice.ino' File
    - Imply the code for the used smart project.

--- SmartDevice/SmartDevice.ino
+++ SmartDevice/SmartDevice.ino
@@ -1,126 +1,64 @@
-// SD Card Module
-#include <SPI.h>
-#include <SD.h>
-
 // Real Time Clock (RTC)
 #include "RTClib.h"
 RTC_Millis rtc;     // Software Real Time Clock (RTC)
 DateTime rightNow;  // used to store the current time.
 
-// SD Card - Confirm Pin
-#define SDpin           10
 
-//Rotary Potentiometer - Confirm Pin
-#define Potentiometer   A3
+// Servo Motor
+#include <Servo.h>
+Servo servo;
 
-//LED Lights - Confirm Pins
-#define LED_RED_PIN     4
-#define LED_YELLOW_PIN  5
-#define LED_GREEN_PIN   6
+int eastLDR = 0;
+int westLDR = 1;
+int east = 0;
+int west = 0;
+int error = 0;
+int calibration = 600;
+int servoposition = 90;
 
 
 void setup() {
 
-  // put your setup code here, to run once:
-
-  // Output the LED Lights so it could show whether the Solar Panel worked perfectly or not.
-  pinMode(LED_RED_PIN, OUTPUT);
-  pinMode(LED_YELLOW_PIN, OUTPUT);
-  pinMode(LED_GREEN_PIN, OUTPUT);
+  servo.attach(9);
+  
   Serial.begin(9600);  // Open serial communications and wait for port to open:
   while (!Serial) {
     delay(1);  // wait for serial port to connect. Needed for native USB port only
   }
+}
 
-  // SD Card initialisation
-  Serial.print("Initializing SD card...");
-  if (!SD.begin(SDpin)) {
-    Serial.println("initialization failed!");
-    while (1)
-      ;
+void loop()
+
+{
+  east = calibration + analogRead(eastLDR);
+  west = analogRead(westLDR);
+  if (east < 350 && west < 350)
+  {
+    while (servoposition <= 150)
+    {
+      servoposition++;
+      servo.write(servoposition);
+      delay(100);
+    }
   }
-  // Real Time Clock (RTC)
-  rtc.begin(DateTime(F(__DATE__), F(__TIME__)));
-  Serial.println("initialization done.");
-  logEvent("System Initialisation...");
+
+  error = east - west;
+  if (error > 15)
+  {
+    if (servoposition <= 150)
+    {
+      servoposition++;
+      servo.write(servoposition);
+    }
+  }
+
+  else if (error < -15)
+  {
+    if (servoposition > 20)
+    {
+      servoposition--;
+      servo.write(servoposition);
+    }
+  }
+  delay(100);
 }
-
-void loop() {
-
-  // put your main code here, to run repeatedly:
-
-  rotaryPotentiometer();
-  servoMotor();
-  LDR();
-  crashSensor();
-  LED_Display();
-  piezoButton();
-
-  delay(500);
-}
-
-/*
-
-Using the Potentiometer, determines at which bytes the device will start to opearated.
-@params none.
-@return none.
-
-*/
-void rotaryPotentiometer() {
-}
-
-/*
-
-If the Servo Motor is activated, rotate the Solar Panel so that it can observe the Sun in multi-direction.
-@params none.
-@return none.
-
-*/
-void servoMotor() {
-}
-
-/*
-
-If the LDRs are activated, detects the highest possible light levels.
-@params none.
-@return none.
-
-*/
-void LDR() {
-}
-
-/*
-
-If the Crash Sensor being pressed, operates the Solar Panel.
-@params none.
-@return none.
-
-*/
-void crashSensor() {
-}
-
-
-/*
-
-When the Solar Panel started to observing and converting the Sun energies into electricity, shows the amount of it onto a LED displayer.
-@params none.
-@return none.
-
-*/
-void LED_Display() {
-}
-
-
-/*
-
-When the Solar Panel detected where the Sun is, play a confirm sound from the Piezo.
-@params none.
-@return none.
-
-*/
-void piezoButton() {
-}
-
-
-// https://circuitdigest.com/microcontroller-projects/building-your-own-sun-tracking-solar-panel-using-arduino link to follow on with the project.
-// https://circuitdigest.com/microcontroller-projects/arduino-solar-panel-tracker another one.
